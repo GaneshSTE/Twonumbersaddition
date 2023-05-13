a67205a4512cca789fb8e5e0037ab83d89835107
ganesh <pganeshkumar.2010@gmail.com>
2023-05-13 15:44:54 +0530

    Remove space

--- src/Twonumbers/Twonumbersaddition.java
+++ src/Twonumbers/Twonumbersaddition.java
@@ -5,7 +5,7 @@ public class Twonumbersaddition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Test Two numbers addition"); 
-       System.out.println("First program");
+       System.out.println("First ");
 	}
 
 }

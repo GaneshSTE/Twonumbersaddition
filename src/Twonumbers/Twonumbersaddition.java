d3fe056881fda1aa402e129186cbcb23655b5bd8
ganesh <pganeshkumar.2010@gmail.com>
2023-05-13 12:15:22 +0530

    Add Two numbers

---
+++ src/Twonumbers/Twonumbersaddition.java
@@ -0,0 +1,10 @@
+package Twonumbers;
+
+public class Twonumbersaddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Test Two numbers addition"); 
+	}
+
+}
